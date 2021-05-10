a4b5762bafb06af152ddc32bf74df8bdf9e1d556
Vladimir Goncharenko <df.201640@yahoo.com>
2021-05-10 12:51:53 +0300

    fixed JsonSaver

--- src/main/java/com/naya/consumer/services/JsonSaverImpl.java
+++ src/main/java/com/naya/consumer/services/JsonSaverImpl.java
@@ -10,11 +10,12 @@ import java.io.File;
 @Slf4j
 public class JsonSaverImpl implements JsonSaver {
 
+  private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     @SneakyThrows
     public void convertAndSave(String path, Quote object) {
         if (object != null) {
-            ObjectMapper objectMapper = new ObjectMapper();
             String str = path + System.nanoTime() + ".txt";
             objectMapper.writeValue(new File(str), object);
             log.info("CONSUMER: Object path: " + str + " was converted to json format");
